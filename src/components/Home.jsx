2156ab41a67cf607a7408b50b443c5ddafd0d037
Nitin Khatri <flickeryfucc@gmail.com>
2024-12-06 00:23:17 +0530

    last changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Home() {
   return (
     <div data-scroll data-scroll-section data-scroll-speed="-.3" className='w-full h-screen   '>
-       <video className='w-full h-screen object-cover' muted autoPlay loop src="/video.mp4">
+       <video className='w-full h-screen object-cover' muted autoPlay loop src="/fashion-website/video.mp4" controls>
        </video>
        
     </div>
